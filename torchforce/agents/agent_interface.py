c4d58e8392007a972ccb211ec26faafd9885038f
nathan <nathan.bruzat@mediamap.ai>
2020-05-23 10:26:45 +0200

    fix pass in agent_interface

--- torchforce/agents/agent_interface.py
+++ torchforce/agents/agent_interface.py
@@ -4,10 +4,13 @@ import abc
 class AgentInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def get_action(self, observation): pass
+    def get_action(self, observation):
+        pass
 
     @abc.abstractmethod
-    def learn(self, observation, action, reward, next_observation) -> None: pass
+    def learn(self, observation, action, reward, next_observation) -> None:
+        pass
 
     @abc.abstractmethod
-    def episode_finished(self) -> None: pass
+    def episode_finished(self) -> None:
+        pass
