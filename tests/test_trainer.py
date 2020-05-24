895e9f98e1bb4ced0021d37e830741e3862334eb
bruzat <nathan.bruzat@viacesi.fr>
2020-05-24 17:53:31 +0200

    Fix test do_episode

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -10,38 +10,38 @@ class FakeEnv(gym.Env):
     def __init__(self):
         super(FakeEnv).__init__()
         self.step_done = False
-        self.reset = False
-        self.render = False
+        self.reset_done = False
+        self.render_done = False
 
     def step(self, action):
         self.step_done = True
-        return None, 0, False, None
+        return True, 0, True, True
 
     def reset(self):
-        self.reset = True
-        return None
+        self.reset_done = True
+        return True
 
     def render(self, mode='human'):
-        self.render = True
+        self.render_done = True
 
 
 class FakeAgent(AgentInterface):
 
     def __init__(self):
-        super(FakeEnv).__init__()
+        super(FakeAgent).__init__()
         self.get_action_done = False
-        self.learn = False
-        self.episode_finished = False
+        self.learn_done = False
+        self.episode_finished_done = False
 
     def get_action(self, observation):
         self.get_action_done = True
-        return None
+        return True
 
     def learn(self, observation, action, reward, next_observation) -> None:
-        self.learn = True
+        self.learn_done = True
 
     def episode_finished(self) -> None:
-        self.episode_finished = True
+        self.episode_finished_done = True
 
 
 def test_arg_to_agent():
@@ -71,9 +71,9 @@ def test_get_agent():
 def test_do_episode():
     fake_env = FakeEnv()
     fake_agent = FakeAgent()
-    assert fake_agent.get_action_done is False and fake_agent.learn is False and fake_agent.episode_finished is False
-    assert fake_env.step_done is False and fake_env.reset is False and fake_env.render is False
+    assert fake_agent.get_action_done is False and fake_agent.learn_done is False and fake_agent.episode_finished_done is False
+    assert fake_env.step_done is False and fake_env.reset_done is False and fake_env.render_done is False
 
     Trainer.do_episode(env=fake_env, agent=fake_agent)
-    assert fake_agent.get_action_done is True and fake_agent.learn is True and fake_agent.episode_finished is True
-    assert fake_env.step_done is True and fake_env.reset is True and fake_env.render is True
+    assert fake_agent.get_action_done is True and fake_agent.learn_done is True and fake_agent.episode_finished_done is True
+    assert fake_env.step_done is True and fake_env.reset_done is True and fake_env.render_done is True
