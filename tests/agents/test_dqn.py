7ce61fb28246beabd6e6e4ba742239475fbc7f60
bruzat <nathan.bruzat@viacesi.fr>
2020-05-27 20:37:53 +0200

    Add simple_Network in dqn

--- tests/agents/test_dqn.py
+++ tests/agents/test_dqn.py
@@ -10,132 +10,145 @@ from torchforce.memories import ExperienceReplay
 
 
 class Network(nn.Module):
-	def __init__(self):
-		super(Network, self).__init__()
-		self.dense = nn.Linear(3, 4)
+    def __init__(self):
+        super(Network, self).__init__()
+        self.dense = nn.Linear(3, 4)
 
-	def forward(self, x):
-		x = self.dense(x)
-		x = F.relu(x)
-		return x
+    def forward(self, x):
+        x = self.dense(x)
+        x = F.relu(x)
+        return x
 
 
 def test_dqn_agent_instantiation():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory)
+    DQN(Discrete(4), Discrete(4), memory, neural_network=network)
 
 
 def test_dqn_agent_instantiation_error_action_space():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	with pytest.raises(TypeError):
-		agent = DQN(None, network, memory)
+    with pytest.raises(TypeError):
+        DQN(None, Discrete(1), memory, neural_network=network)
+
+
+def test_dqn_agent_instantiation_error_observation_space():
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
+
+    with pytest.raises(TypeError):
+        DQN(Discrete(1), None, memory, neural_network=network)
 
 
 def test_dqn_agent_instantiation_error_neural_network():
-	memory = ExperienceReplay(max_size=5)
+    memory = ExperienceReplay(max_size=5)
 
-	with pytest.raises(TypeError):
-		agent = DQN(Discrete(4), None, memory)
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), memory, neural_network=154)
+
+    DQN(Discrete(4), Discrete(4), memory, neural_network=None)
 
 
 def test_dqn_agent_instantiation_error_memory():
-	network = Network()
+    network = Network()
 
-	with pytest.raises(TypeError):
-		agent = DQN(Discrete(4), network, None)
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), None, neural_network=network)
 
 
 def test_dqn_agent_instantiation_error_loss():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	with pytest.raises(TypeError):
-		agent = DQN(Discrete(4), network, memory, loss="LOSS_ERROR")
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), memory, neural_network=network, loss="LOSS_ERROR")
 
 
 def test_dqn_agent_instantiation_error_optimizer():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	with pytest.raises(TypeError):
-		agent = DQN(Discrete(4), network, memory, optimizer="OPTIMIZER_ERROR")
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), memory, neural_network=network, optimizer="OPTIMIZER_ERROR")
 
 
 def test_dqn_agent_instantiation_error_greedy_exploration():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	with pytest.raises(TypeError):
-		agent = DQN(Discrete(4), network, memory, greedy_exploration="GREEDY_EXPLORATION_ERROR")
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), memory, neural_network=network, greedy_exploration="GREEDY_EXPLORATION_ERROR")
 
 
 def test_dqn_agent_instantiation_custom_loss():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory, loss=nn.MSELoss())
+    DQN(Discrete(4), Discrete(4), memory, neural_network=network, loss=nn.MSELoss())
 
 
 def test_dqn_agent_instantiation_custom_optimizer():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory, optimizer=optim.RMSprop(network.parameters()))
+    DQN(Discrete(4), Discrete(4), memory, neural_network=network, optimizer=optim.RMSprop(network.parameters()))
+
+    with pytest.raises(TypeError):
+        DQN(Discrete(4), Discrete(4), memory, neural_network=None, optimizer=optim.RMSprop(network.parameters()))
 
 
 def test_dqn_agent_getaction():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory, greedy_exploration=Greedy())
+    agent = DQN(Discrete(4), Discrete(4), memory, neural_network=network, greedy_exploration=Greedy())
 
-	observation = [0.0, 0.5, 1.]
+    observation = [0.0, 0.5, 1.]
 
-	agent.get_action(observation)
+    agent.get_action(observation)
 
 
 def test_dqn_agent_getaction_non_greedy():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory, greedy_exploration=EpsilonGreedy(1.))
+    agent = DQN(Discrete(4), Discrete(4), memory, neural_network=network, greedy_exploration=EpsilonGreedy(1.))
 
-	observation = [0.0, 0.5, 1.]
+    observation = [0.0, 0.5, 1.]
 
-	agent.get_action(observation)
+    agent.get_action(observation)
 
 
 def test_dqn_agent_learn():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory)
+    agent = DQN(Discrete(4), Discrete(4), memory, neural_network=network)
 
-	obs = [1, 2, 5]
-	action = 0
-	reward = 0
-	next_obs = [5, 9, 4]
-	done = False
+    obs = [1, 2, 5]
+    action = 0
+    reward = 0
+    next_obs = [5, 9, 4]
+    done = False
 
-	obs_s = [obs, obs, obs]
-	actions = [1, 2, 3]
-	rewards = [-2.2, 5, 4]
-	next_obs_s = [next_obs, next_obs, next_obs]
-	dones = [False, True, False]
+    obs_s = [obs, obs, obs]
+    actions = [1, 2, 3]
+    rewards = [-2.2, 5, 4]
+    next_obs_s = [next_obs, next_obs, next_obs]
+    dones = [False, True, False]
 
-	memory.extend(obs_s, actions, rewards, next_obs_s, dones)
+    memory.extend(obs_s, actions, rewards, next_obs_s, dones)
 
-	agent.learn(obs, action, reward, next_obs, done)
-	agent.learn(obs, action, reward, next_obs, done)
+    agent.learn(obs, action, reward, next_obs, done)
+    agent.learn(obs, action, reward, next_obs, done)
 
 
 def test_dqn_agent_episode_finished():
-	network = Network()
-	memory = ExperienceReplay(max_size=5)
+    network = Network()
+    memory = ExperienceReplay(max_size=5)
 
-	agent = DQN(Discrete(4), network, memory)
-	agent.episode_finished()
+    agent = DQN(Discrete(4), Discrete(4), memory, neural_network=network)
+    agent.episode_finished()
--- torchforce/agents/dqn.py
+++ torchforce/agents/dqn.py
@@ -1,22 +1,34 @@
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from gym.spaces import Discrete
+from gym.spaces import Discrete, flatdim
 
 from torchforce.agents import AgentInterface
 from torchforce.explorations import GreedyExplorationInterface, EpsilonGreedy
 from torchforce.memories import MemoryInterface
+from torchforce.networks import SimpleNetwork
 
 
 class DQN(AgentInterface):
 
-    def __init__(self, action_space, neural_network, memory, step_train=2, batch_size=8, gamma=0.99, loss=None,
+    def __init__(self, action_space, observation_space, memory, neural_network=None, step_train=2, batch_size=8,
+                 gamma=0.99, loss=None,
                  optimizer=None, greedy_exploration=None):
 
         if not isinstance(action_space, Discrete):
             raise TypeError(
                 "action_space need to be instance of gym.spaces.Space.Discrete, not :" + str(type(action_space)))
+        if not isinstance(observation_space, Discrete):
+            raise TypeError(
+                "observation_space need to be instance of gym.spaces.Space.Discrete, not :" + str(
+                    type(observation_space)))
 
+        if neural_network is None and optimizer is not None:
+            raise TypeError("If neural_network is None, optimizer need to be None not " + str(type(optimizer)))
+
+        if neural_network is None:
+            neural_network = SimpleNetwork(observation_shape=flatdim(observation_space),
+                                           action_shape=flatdim(action_space))
         if not isinstance(neural_network, torch.nn.Module):
             raise TypeError("neural_network need to be instance of torch.nn.Module, not :" + str(type(neural_network)))
 
