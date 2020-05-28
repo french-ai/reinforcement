f53ff17c30feab79b30309bd18a84abbbe9c1626
garaytc <charlie.garayt@hotmail.fr>
2020-05-28 20:42:23 +0200

    Add c51 network

---
+++ torchforce/networks/c51_network.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch.nn as nn
+import torch
+
+from torchforce.networks import BaseNetwork
+
+from gym.spaces import Space, Discrete
+
+class C51Network(BaseNetwork):
+    def __init__(self, observation_shape, action_shape):
+        super().__init__(observation_shape=observation_shape, action_shape=action_shape)
+
+        if not isinstance(observation_shape, (tuple, int)):
+            raise TypeError("observation_space need to be Space not " + str(type(observation_shape)))
+        if not isinstance(action_shape, (tuple, int)):
+            raise TypeError("action_space need to be Space not " + str(type(action_shape)))
+        
+        self.NUM_ATOMS = 51
+        
+        self.network = nn.Sequential()
+        self.network.add_module("C51_Linear_Input", nn.Linear(np.prod(self.observation_space), 64))
+        self.network.add_module("C51_LeakyReLU_Input", nn.LeakyReLU())
+        self.network.add_module("C51_Linear_1", nn.Linear(64, 64))
+        self.network.add_module("C51_LeakyReLU_1", nn.LeakyReLU())
+
+        self.distributional_list = []
+        self.len_distributional = np.prod(self.action_space)
+
+        for i in range(self.len_distributional):
+            distributional = nn.Sequential()
+            distributional.add_module("C51_Distributional_" + str(i) + "_Linear", nn.Linear(64, self.NUM_ATOMS))
+            distributional.add_module("C51_Distributional_" + str(i) + "_Softmax", nn.Softmax(dim=1))
+
+            self.distributional_list.append(distributional)
+        
+    def forward(self, observation):
+        x = observation.view(observation.shape[0], -1)
+        x = self.network(x)  
+        
+        Q = [distributionalLayer(x) for distributionalLayer in self.distributional_list]
+        Q = torch.cat(Q)
+        Q = torch.reshape(Q, (self.len_distributional, -1, self.NUM_ATOMS))
+        Q = Q.permute(1, 0, 2)
+        
+        return Q
