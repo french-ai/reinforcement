64062e397226da3fe7ebde32e7b4edfd49152f60
garaytc <charlie.garayt@hotmail.fr>
2020-05-28 21:11:58 +0200

    Implement distributional DQN

--- torchforce/agents/distributional_dqn.py
+++ torchforce/agents/distributional_dqn.py
@@ -8,7 +8,7 @@ from gym.spaces import Discrete, Space, flatdim, flatten
 from torchforce.agents import DQN
 from torchforce.explorations import GreedyExplorationInterface, EpsilonGreedy
 from torchforce.memories import MemoryInterface, ExperienceReplay
-from torchforce.networks import SimpleNetwork
+from torchforce.networks import C51Network
 
 
 class DistributionalDQN(DQN):
@@ -17,26 +17,32 @@ class DistributionalDQN(DQN):
                  loss=None, optimizer=None, greedy_exploration=None):
 
         super().__init__(action_space, observation_space, memory, neural_network, step_train, batch_size, gamma, loss, optimizer, greedy_exploration)
-        
-        self.step_copy = step_copy
-
-        self.num_atoms = num_atoms
-        self.r_min = r_min
-        self.r_max = r_max
-
-        self.delta_support = (r_max - r_min) / float(num_atoms - 1)
-        self.support = [r_min + i * delta_supports for i in range(num_atoms)]
-
+               
+        if neural_network is None:
+            self.neural_network = C51Network(observation_shape=flatdim(observation_space),
+                                           action_shape=flatdim(action_space))
         if loss is None:
             self.loss = torch.nn.CrossEntropyLoss()
 
         if optimizer is None:
             self.optimizer = optim.Adam(self.neural_network.parameters(), lr=0.01)
 
+        self.num_atoms = num_atoms
+        self.r_min = r_min
+        self.r_max = r_max
+
+        self.delta_support = (r_max - r_min) / float(num_atoms - 1)
+        self.support = torch.Tensor([r_min + i * self.delta_support for i in range(num_atoms)])
+
     def get_action(self, observation):
 
-        prediction = self.neural_network.forward(observation)
-        q = prediction * z
+        observation = torch.tensor([flatten(self.observation_space, observation)])
+
+        prediction = self.neural_network.forward(observation).detach()[0]
+        q_values = prediction * self.support
+        q_values = torch.sum(q_values, dim=1)
+
+        return torch.argmax(q_values).detach().item()
 
     def learn(self, observation, action, reward, next_observation, done) -> None:
         pass
