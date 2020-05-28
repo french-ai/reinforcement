ebb7bfa8fa3b06837084f905d4d37d670e91a1fb
garaytc <charlie.garayt@hotmail.fr>
2020-05-29 00:17:24 +0200

    Implement train()

--- torchforce/agents/distributional_dqn.py
+++ torchforce/agents/distributional_dqn.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from math import floor, ceil
 
 import torch
 import torch.nn.functional as F
@@ -21,6 +22,8 @@ class DistributionalDQN(DQN):
         if neural_network is None:
             self.neural_network = C51Network(observation_shape=flatdim(observation_space),
                                            action_shape=flatdim(action_space))
+            num_atoms = 51
+
         if loss is None:
             self.loss = torch.nn.CrossEntropyLoss()
 
@@ -31,21 +34,45 @@ class DistributionalDQN(DQN):
         self.r_min = r_min
         self.r_max = r_max
 
-        self.delta_support = (r_max - r_min) / float(num_atoms - 1)
-        self.support = torch.Tensor([r_min + i * self.delta_support for i in range(num_atoms)])
+        self.delta_z = (r_max - r_min) / float(num_atoms - 1)
+        self.z = torch.Tensor([r_min + i * self.delta_z for i in range(num_atoms)])
 
     def get_action(self, observation):
 
         observation = torch.tensor([flatten(self.observation_space, observation)])
 
         prediction = self.neural_network.forward(observation).detach()[0]
-        q_values = prediction * self.support
+        q_values = prediction * self.z
         q_values = torch.sum(q_values, dim=1)
 
         return torch.argmax(q_values).detach().item()
 
-    def learn(self, observation, action, reward, next_observation, done) -> None:
-        pass
+    def train(self):
+        
+        self.batch_size = 3
+        observations, actions, rewards, next_observations, dones = self.memory.sample(self.batch_size)
 
-    def train(self):        
-        pass
+        predictions_next = self.neural_network.forward(next_observations).detach()
+        q_values_next = predictions_next * self.z
+        q_values_next = torch.sum(q_values_next, dim=2)
+
+        actions_next = torch.argmax(q_values_next, dim=1)
+
+        m_prob = torch.zeros((self.batch_size, self.action_space.n, self.num_atoms))
+
+        for sample_i in range(self.batch_size):
+            done = dones[sample_i]
+            for j in range(self.num_atoms):
+                Tzj = torch.clamp(rewards[sample_j] + self.gamma * self.z[j] * (1 - done), self.r_min, self.r_max)
+                bj = (Tzj - self.r_min) / self.delta_z
+                l, u = floor(bj), ceil(bj)
+                m_prob[sample_i][l] += (done + (1 - done) * predictions_next[sample_i][actions_next[sample_i]][j] * (u - bj))
+                m_prob[sample_i][u] += (done + (1 - done) * predictions_next[sample_i][actions_next[sample_i]][j] * (bj - l))
+
+            if done:
+                break
+
+        self.optimizer.zero_grad()
+        loss = self.loss(q_predict[0], m_prob)
+        loss.backward()
+        self.optimizer.step()
