e62784173f4c5aa1226d9b3dee8a92021b6351b5
bruzat <nathan.bruzat@viacesi.fr>
2020-06-16 11:17:42 +0200

    Add docs for Exploration

--- torchforce/explorations/adaptative_epsilon_greedy.py
+++ torchforce/explorations/adaptative_epsilon_greedy.py
@@ -4,12 +4,16 @@ from torchforce.explorations import EpsilonGreedy
 class AdaptativeEpsilonGreedy(EpsilonGreedy):
 
     def __init__(self, epsilon_max, epsilon_min, step_max, step_min=0):
-        """
+        """ Create AdaptativeEpsilonGreedy
 
-        :param epsilon_max:
-        :param epsilon_min:
-        :param step_max:
-        :param step_min:
+        :param epsilon_max: value for start exploration
+        :type epsilon_min: float [0.0,1.0], epsilon_max>epsilon_min
+        :param epsilon_min: min value exploration
+        :type epsilon_min: float [0.0,1.0], epsilon_min<epsilon_max
+        :param step_max: step where epsilon start to decrease
+        :type step_max: int
+        :param step_min: step where greedy return always False
+        :type step_min: int
         """
         super().__init__(epsilon_max)
         self.epsilon_max = epsilon_max
@@ -18,10 +22,10 @@ class AdaptativeEpsilonGreedy(EpsilonGreedy):
         self.step_min = step_min
 
     def be_greedy(self, step):
-        """
+        """ Return greedy
 
-        :param step:
-        :return:
+        :param step: id of step
+        :type step: int
         """
         if step <= self.step_min:
             return False
--- torchforce/explorations/epsilon_greedy.py
+++ torchforce/explorations/epsilon_greedy.py
@@ -6,17 +6,18 @@ from torchforce.explorations import GreedyExplorationInterface
 class EpsilonGreedy(GreedyExplorationInterface):
 
     def __init__(self, epsilon):
-        """
+        """ Create EpsilonGreedy
 
-        :param epsilon:
+        :param epsilon: value for threshold greedy
+        :type epsilon: float
         """
         self.epsilon = epsilon
 
     def be_greedy(self, step):
-        """
+        """ Return True if random() > on self.epsilon
 
-        :param step:
-        :return:
+        :param step: id of step
+        :type step: int
         """
         return random() > self.epsilon
 
