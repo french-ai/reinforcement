91a9516cf1fe7d5a6c4476ea2c165b1a1c016515
bruzat <nathan.bruzat@viacesi.fr>
2020-07-01 13:36:36 +0200

    Refactor result for generate test for all agent with more parameters

--- results/result.py
+++ results/result.py
@@ -9,7 +9,7 @@ from torchforce import Trainer
 from torchforce.agents import DQN, DoubleDQN, DuelingDQN, CategoricalDQN
 from torchforce.explorations import EpsilonGreedy, AdaptativeEpsilonGreedy
 from torchforce.memories import ExperienceReplay
-from torchforce.networks import SimpleNetwork, SimpleDuelingNetwork
+from torchforce.networks import SimpleNetwork, SimpleDuelingNetwork, C51Network
 
 memory = [ExperienceReplay]
 step_train = [1, 2, 10]
@@ -26,28 +26,71 @@ greedy_exploration = [EpsilonGreedy(0.1),
                       AdaptativeEpsilonGreedy(0.6, 0.1, 50000, 0),
                       AdaptativeEpsilonGreedy(0.8, 0.1, 50000, 0)]
 
-arg_all = [{"agent": [DQN, DoubleDQN, DuelingDQN, CategoricalDQN],
-            "memory": memory,
-            "neural_network": [SimpleNetwork],
-            "step_train": step_train,
-            "batch_size": batch_size,
-            "gamma": gamma,
-            "loss": loss,
-            "optimizer": optimizer,
-            "lr": lr,
-            "greedy_exploration": greedy_exploration
+arg_all = [{"agent": {"class": [DQN, DoubleDQN],
+                      "param": {"step_train": step_train,
+                                "batch_size": batch_size,
+                                "gamma": gamma,
+                                "greedy_exploration": greedy_exploration
+                                }
+                      },
+            "neural_network": {"class": [SimpleNetwork],
+                               "param": {}},
+            "optimizer": {"class": optimizer,
+                          "param": {"lr": lr}},
+            "memory": {"class": memory,
+                       "param": {"max_size": [100, 300, 500]}},
             },
-           {"agent": [DuelingDQN],
-            "memory": memory,
-            "neural_network": [SimpleDuelingNetwork],
-            "step_train": step_train,
-            "batch_size": batch_size,
-            "gamma": gamma,
-            "loss": loss,
-            "optimizer": optimizer,
-            "lr": lr,
-            "greedy_exploration": greedy_exploration
+           {"agent": {"class": [DuelingDQN],
+                      "param": {"step_train": step_train,
+                                "batch_size": batch_size,
+                                "gamma": gamma,
+                                "greedy_exploration": greedy_exploration
+                                }
+                      },
+            "neural_network": {"class": [SimpleDuelingNetwork],
+                               "param": {}},
+            "optimizer": {"class": optimizer,
+                          "param": {"lr": lr}},
+            "memory": {"class": memory,
+                       "param": {"max_size": [100, 300, 500]}},
+            },
+           {"agent": {"class": [CategoricalDQN],
+                      "param": {"step_train": step_train,
+                                "batch_size": batch_size,
+                                "gamma": gamma,
+                                "greedy_exploration": greedy_exploration
+                                }
+                      },
+            "neural_network": {"class": [C51Network],
+                               "param": {}},
+            "optimizer": {"class": optimizer,
+                          "param": {"lr": lr}},
+            "memory": {"class": memory,
+                       "param": {"max_size": [100, 300, 500]}},
             }]
+
+
+def mzip(*iterables):
+    iterators = [iter(it) for it in iterables]
+    if not iterators:
+        return []
+    for elem in iterators[0]:
+        if len(iterators) == 1:
+            yield [elem]
+        else:
+            for elems in mzip(*iterables[1:]):
+                yield [elem, *elems]
+
+
+def dict_mzip(x):
+    if not x.values():
+        yield {}
+    for values in mzip(*list(x.values())):
+        param = {}
+        for key, val in zip(x.keys(), values):
+            param[key] = val
+        yield param
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -57,43 +100,47 @@ if __name__ == '__main__':
                         default=False)
     args = parser.parse_args()
 
+    env = gym.make(args.env)
+
     for arg in arg_all:
-        for agent_class in arg["agent"]:
-            for memory in arg["memory"]:
-                for neural_network_class in arg["neural_network"]:
-                    for step_train in arg["step_train"]:
-                        for batch_size in arg["batch_size"]:
-                            for gamma in arg["gamma"]:
-                                for loss in arg["loss"]:
-                                    for optimizer in arg["optimizer"]:
-                                        for lr in arg["lr"]:
-                                            for greedy_exploration in arg["greedy_exploration"]:
-                                                log_dir = str(args.env) + "/" + str(agent_class.__name__) + "/" + str(
-                                                    memory.__name__) + "_" + str(
-                                                    neural_network_class.__name__) + "_" + str(
-                                                    step_train) + "_" + str(batch_size) + "_" + str(gamma) + "_" + str(
-                                                    loss) + "_" + str(optimizer.__name__) + "_" + str(lr) + "_" + str(
-                                                    greedy_exploration) + "/"
+        arg_agent = arg["agent"]
+        arg_neural_network = arg["neural_network"]
+        arg_optimizer = arg["optimizer"]
+        arg_memory = arg["memory"]
 
-                                                env = gym.make(args.env)
+        for class_neural_network in arg_neural_network["class"]:
+            for param_network in dict_mzip(arg_neural_network["param"]):
 
-                                                neural_network = neural_network_class(
-                                                    observation_shape=flatdim(env.observation_space),
-                                                    action_shape=flatdim(env.action_space)).cpu()
+                for class_optimizer in arg_optimizer["class"]:
+                    for param_optimizer in dict_mzip(arg_optimizer["param"]):
 
-                                                agent = agent_class(observation_space=env.observation_space,
-                                                                    action_space=env.action_space,
-                                                                    memory=memory(), neural_network=neural_network,
-                                                                    step_train=step_train, batch_size=batch_size,
-                                                                    gamma=gamma,
-                                                                    loss=loss,
-                                                                    optimizer=optimizer(
-                                                                        params=neural_network.parameters(),
-                                                                        lr=lr),
-                                                                    greedy_exploration=greedy_exploration)
+                        for class_agent in arg_agent["class"]:
+                            for param_agent in dict_mzip(arg_agent["param"]):
 
-                                                trainer = Trainer(environment=env, agent=agent,
-                                                                  log_dir=log_dir)
-                                                trainer.train(max_episode=args.max_episode, render=args.render)
+                                for class_memory in arg_memory["class"]:
+                                    for param_memory in dict_mzip(arg_memory["param"]):
+                                        neural_network = class_neural_network(
+                                            observation_shape=flatdim(env.observation_space),
+                                            action_shape=flatdim(env.action_space),
+                                            **param_network)
+                                        optimizer = class_optimizer(neural_network.parameters(), **param_optimizer)
 
-                                                agent.save(file_name="save.p", dire_name=log_dir)
+                                        memory = class_memory(**param_memory)
+
+                                        agent = class_agent(observation_space=env.observation_space,
+                                                            action_space=env.action_space,
+                                                            neural_network=neural_network,
+                                                            optimizer=optimizer, memory=memory,
+                                                            **param_agent)
+
+                                        log_dir = args.env + "/" + class_agent.__name__ + "/" + "-".join(
+                                            [str(x) for x in list(param_agent.values())]) + "_" + "-".join(
+                                            [str(x) for x in list(param_network.values())]) + "_" + "-".join(
+                                            [str(x) for x in list(param_optimizer.values())]) + "_" + "-".join(
+                                            [str(x) for x in list(param_memory.values())])
+
+                                        trainer = Trainer(environment=env, agent=agent,
+                                                          log_dir=log_dir)
+                                        # trainer.train(max_episode=args.max_episode, render=args.render)
+
+                                        agent.save(file_name="save.p", dire_name=log_dir)
